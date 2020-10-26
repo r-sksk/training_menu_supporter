d12695a2b7d9294b0101fe09677512dd6d79cf38
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-10-26 15:11:34 +0900

    ヘッダー追加、投稿削除機能追加、ユーザーページに投稿一覧追加

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -29,6 +29,12 @@ class MenusController < ApplicationController
     redirect_to menu_path(@menu)
   end
 
+  def destroy
+    menu = Menu.find(params[:id])
+    menu.destroy
+    redirect_to menus_path
+  end
+
   private
    def menu_params
     params.require(:menu).permit(:title, :body, :image)
