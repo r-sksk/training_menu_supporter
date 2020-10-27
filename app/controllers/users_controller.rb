6cb0ab68d89e3ae50789bf8f71231d63780030b1
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-10-27 13:53:15 +0900

    コントローラにbefore_action追加、トップページのレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]  #ログインしていないユーザーはindex画面しか閲覧できない→ログイン画面へ
   def index
     @users = User.all
   end
