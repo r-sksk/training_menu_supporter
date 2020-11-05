114e916694c88921bce0ec00d222894f0f143e59
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-11-05 13:26:36 +0900

    ゲストログイン機能リファクタリング、README編集

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+class SessionsController < Devise::SessionsController
+  def new_guest
+    user = User.find_or_create_by(username: 'Guest', email: 'guest@example.com') do |user| #ゲストユーザーがあれば取り出し、なければ作成する
+      user.password = SecureRandom.urlsafe_base64 #ランダムパスワードの作成
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+   end
+end
